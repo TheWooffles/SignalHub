891dea5303f7f36644bd47737f30c8f4e4c14316
TheWooffles <95188174+TheWooffles@users.noreply.github.com>
2024-09-17 20:51:10 -0400

    Update scriptfov.lua

--- scriptfov.lua
+++ scriptfov.lua
@@ -1,48 +1,19 @@
-local Players = game:GetService("Players"):GetChildren()
-local RunService = game:GetService("RunService")
-local highlight = Instance.new("Highlight")
-highlight.Name = "Highlight"
+local esp = loadstring(game:HttpGet('https://raw.githubusercontent.com/0f76/seere_v3/main/ESP/v3_esp.lua'))()
 
-for i, v in pairs(Players) do
-    repeat wait() until v.Character
-    if not v.Character:FindFirstChild("HumanoidRootPart"):FindFirstChild("Highlight") then
-        local highlightClone = highlight:Clone()
-        highlightClone.Adornee = v.Character
-        highlightClone.Parent = v.Character:FindFirstChild("HumanoidRootPart")
-        highlightClone.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
-        highlightClone.Name = "Highlight"
-    end
-end
+esp.enabled = true
 
-game.Players.PlayerAdded:Connect(function(player)
-    repeat wait() until player.Character
-    if not player.Character:FindFirstChild("HumanoidRootPart"):FindFirstChild("Highlight") then
-        local highlightClone = highlight:Clone()
-        highlightClone.Adornee = player.Character
-        highlightClone.Parent = player.Character:FindFirstChild("HumanoidRootPart")
-        highlightClone.Name = "Highlight"
-    end
-end)
+esp.teamcheck = false
 
-game.Players.PlayerRemoving:Connect(function(playerRemoved)
-    playerRemoved.Character:FindFirstChild("HumanoidRootPart").Highlight:Destroy()
-end)
+esp.outlines = true
+esp.shortnames = true
+esp.team_boxes = {true,Color3.fromRGB(255,255,255),Color3.fromRGB(1,1,1),0}
+esp.team_chams = {true,Color3.fromRGB(138, 139, 194),Color3.fromRGB(138, 139, 194),.25,.75,true}
+esp.team_names = {true,Color3.fromRGB(255,255,255)}
+esp.team_weapon = { true, Color3.fromRGB(255,255,255)}
+esp.team_distance = true
+esp.team_health = true
 
-RunService.Heartbeat:Connect(function()
-    for i, v in pairs(Players) do
-        repeat wait() until v.Character
-        if not v.Character:FindFirstChild("HumanoidRootPart"):FindFirstChild("Highlight") then
-            local highlightClone = highlight:Clone()
-            highlightClone.Adornee = v.Character
-            highlightClone.Parent = v.Character:FindFirstChild("HumanoidRootPart")
-            highlightClone.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
-            highlightClone.Name = "Highlight"
-            task.wait()
-        end
-end
-end)
-
-local fov = 25
+local fov = 20
 local RunService = game:GetService("RunService")
 local UserInputService = game:GetService("UserInputService")
 local Players = game:GetService("Players")
@@ -51,60 +22,60 @@ local Cam = game.Workspace.CurrentCamera
 local FOVring = Drawing.new("Circle")
 FOVring.Visible = true
 FOVring.Thickness = 2
-FOVring.Color = Color3.fromRGB(255, 255, 255) -- Purple color
+FOVring.Color = Color3.fromRGB(255, 255, 255) -- White color
 FOVring.Filled = false
 FOVring.Radius = fov
 FOVring.Position = Cam.ViewportSize / 2
 
 local function updateDrawings()
-    local camViewportSize = Cam.ViewportSize
+        local camViewportSize = Cam.ViewportSize
         FOVring.Position = camViewportSize / 2
         end
 
         local function onKeyDown(input)
-            if input.KeyCode == Enum.KeyCode.Delete then
-                    RunService:UnbindFromRenderStep("FOVUpdate")
-                            FOVring:Remove()
+                if input.KeyCode == Enum.KeyCode.Delete then
+                        RunService:UnbindFromRenderStep("FOVUpdate")
+                                FOVring:Remove()
                                 end
                                 end
 
-                                UserInputService.InputBegan:Connect(onKeyDown)
+                                        UserInputService.InputBegan:Connect(onKeyDown)
 
-                                local function lookAt(target)
-                                    local lookVector = (target - Cam.CFrame.Position).unit
-                                        local newCFrame = CFrame.new(Cam.CFrame.Position, Cam.CFrame.Position + lookVector)
-                                            Cam.CFrame = newCFrame
-                                            end
+                                        local function lookAt(target)
+                                            local lookVector = (target - Cam.CFrame.Position).unit
+                                                local newCFrame = CFrame.new(Cam.CFrame.Position, Cam.CFrame.Position + lookVector)
+                                                    Cam.CFrame = newCFrame
+                                                    end
 
-                                            local function getClosestPlayerInFOV(trg_part)
-                                                local nearest = nil
-                                                    local last = math.huge
-                                                        local playerMousePos = Cam.ViewportSize / 2
+                                                    local function getClosestPlayerInFOV(trg_part)
+                                                        local nearest = nil
+                                                            local last = math.huge
+                                                                local playerMousePos = Cam.ViewportSize / 2
 
-                                                            for _, player in ipairs(Players:GetPlayers()) do
-                                                                    if player ~= Players.LocalPlayer then
-                                                                                local part = player.Character and player.Character:FindFirstChild(trg_part)
-                                                                                            if part then
-                                                                                                            local ePos, isVisible = Cam:WorldToViewportPoint(part.Position)
-                                                                                                                            local distance = (Vector2.new(ePos.x, ePos.y) - playerMousePos).Magnitude
+                                                                    for _, player in ipairs(Players:GetPlayers()) do
+                                                                            if player ~= Players.LocalPlayer then
+                                                                                        local part = player.Character and player.Character:FindFirstChild(trg_part)
+                                                                                                    if part then
+                                                                                                                    local ePos, isVisible = Cam:WorldToViewportPoint(part.Position)
+                                                                                                                                    local distance = (Vector2.new(ePos.x, ePos.y) - playerMousePos).Magnitude
 
-                                                                                                                                            if distance < last and isVisible and distance < fov then
-                                                                                                                                                                last = distance
-                                                                                                                                                                                    nearest = player
-                                                                                                                                                                                            if Player.Team ~= LocalPlayer.Team then 
-                                                                                                                                                                                                return
-                                                                                                                                                                                                    end
-                                                                                                                                                                                                                end
+                                                                                                                                                    if distance < last and isVisible and distance < fov then
+                                                                                                                                                                        last = distance
+                                                                                                                                                                                            nearest = player
+                                                                                                                                                                                                    if Player.Team ~= LocalPlayer.Team then 
+                                                                                                                                                                                                        return
+                                                                                                                                                                                                            end
                                                                                                                                                                                                                         end
-                                                                                                                                                                                                                            end
-
-                                                                                                                                                                                                                                return nearest
                                                                                                                                                                                                                                 end
+                                                                                                                                                                                                                                    end
 
-                                                                                                                                                                                                                                RunService.RenderStepped:Connect(function()
-                                                                                                                                                                                                                                    updateDrawings()
-                                                                                                                                                                                                                                        local closest = getClosestPlayerInFOV("Head")
-                                                                                                                                                                                                                                            if closest and closest.Character:FindFirstChild("Head") then
-                                                                                                                                                                                                                                                    lookAt(closest.Character.Head.Position)
-                                                                                                                                                                                                                                                        end
-                                                                                                                                                                                                                                                        end)
+                                                                                                                                                                                                                                        return nearest
+                                                                                                                                                                                                                                        end
+
+                                                                                                                                                                                                                                        RunService.RenderStepped:Connect(function()
+                                                                                                                                                                                                                                            updateDrawings()
+                                                                                                                                                                                                                                                local closest = getClosestPlayerInFOV("Head")
+                                                                                                                                                                                                                                                    if closest and closest.Character:FindFirstChild("Head") then
+                                                                                                                                                                                                                                                            lookAt(closest.Character.Head.Position)
+                                                                                                                                                                                                                                                                end
+                                                                                                                                                                                                                                                                end)
